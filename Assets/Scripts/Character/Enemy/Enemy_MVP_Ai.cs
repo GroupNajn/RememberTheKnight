e03d2344bf667d918cd06879c5158c974190405d
Henric Nilsson <henric.nilsson01@gmail.com>
2026-03-05 15:33:16 +0100

    Added simple wall and Patrolpoints
    Added a Cube GameObject that acts as a wall.
    
    Added transforms as PatrolPoints in the world which the enemy AI will randomly seelct and patrol inbetween.

--- Assets/Scripts/Character/Enemy/Enemy_MVP_Ai.cs
+++ Assets/Scripts/Character/Enemy/Enemy_MVP_Ai.cs
@@ -1,10 +1,21 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy_MVP_Ai : MonoBehaviour
 {
+
+    // Lukas and Henric contributed 2026-03-05
     [SerializeField] GameObject player;
-    [SerializeField] float speed = 5f;
+    [SerializeField] List<Transform> targetsToFollow;
+    //[SerializeField] Transform targetToFollow;
+    [SerializeField] float distanceToChangeTarget = 1f;
+    [SerializeField] float detectionRange = 5f;
+    [SerializeField] float loseTargetRange = 10f;
+
+    bool isFollowingPlayer;
+    int currentTargetIndex;
     NavMeshAgent agent;
     void Awake()
     {
@@ -20,6 +31,30 @@ public class Enemy_MVP_Ai : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+
+        if(Vector3.Distance(transform.position, player.transform.position) <= detectionRange)
+        {
+            isFollowingPlayer = true;
+        
+        }
+        
+
+        if (Vector3.Distance(transform.position, player.transform.position) > loseTargetRange)
+        {
+            isFollowingPlayer = false;
+        }
+
+        if(!isFollowingPlayer && agent.remainingDistance <= distanceToChangeTarget)
+        {
+            int randomIndex = Random.Range(0, targetsToFollow.Count);
+            currentTargetIndex = randomIndex;
+            agent.SetDestination(targetsToFollow[currentTargetIndex].position);
+        }
+        else if (isFollowingPlayer)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+
+            //agent.SetDestination(player.transform.position);
     }
 }
