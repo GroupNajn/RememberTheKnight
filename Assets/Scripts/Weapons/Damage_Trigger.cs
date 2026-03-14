d7514a501e74ce9aeae9750408460c9344fe1ea9
Lukas <lukas.karlsson2006@gmail.com>
2026-03-14 10:39:08 +0100

    Player take damage script

--- Assets/Scripts/Weapons/Damage_Trigger.cs
+++ Assets/Scripts/Weapons/Damage_Trigger.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class Damage_Trigger : MonoBehaviour
 {
-    // Made by Lukas and Anton A 2026-03-06
+    // Made by Lukas and Anton B 2026-03-06
 
     [SerializeField] int damageAmount = 10;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter");
         IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
         if (damageable != null)
         {
