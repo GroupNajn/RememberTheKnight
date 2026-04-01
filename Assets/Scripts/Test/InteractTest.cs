28a977affdca8ab04168a8c7f8e635f41d222ea0
WilmerJonsson <wimse03@gmail.com>
2026-04-01 16:39:39 +0200

    higher walls, scenemanager

--- Assets/Scripts/Test/InteractTest.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class InteractTest : MonoBehaviour, IInteractable
-{
-    public void Interact()
-    {
-        Debug.Log("Interacted with " + gameObject.name);
-    }
-}
