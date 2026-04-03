8948717110314e1b9dd8845fd31de35431470a48
Henric Nilsson <henric.nilsson01@gmail.com>
2026-04-04 00:29:37 +0200

    Revert "Reverted commits and Readded"
    This reverts commit d8b20ff43a82fec21b01397d0e8b451272f2d2e1.

--- Assets/Scripts/Manager/LootManager.cs
+++ Assets/Scripts/Manager/LootManager.cs
@@ -19,7 +19,12 @@ public class LootManager : MonoBehaviour
     public HashSet<Loot> DroppedLoot {get {return droppedLoot;}}
     HashSet<Loot> droppedLoot;
     [SerializeField] Loot soulPrefab;
-    [SerializeField] List<float> amountChanceTable;
+
+    //[SerializeField] float oneItemDropChance;
+    //[SerializeField] float twoItemDropChance;
+    //[SerializeField] float threeItemDropChance;
+
+
 
     /*
      * Managers need to be initialized via Awake to get priority,
@@ -45,7 +50,7 @@ public class LootManager : MonoBehaviour
         }
 
         //Debug.Log("LootManager subscribed");
-        Event_System.instance.OnEnemyKilled += RollMultipuleLoot;
+        Event_System.instance.OnEnemyKilled += GetOneRandomItemLoot;
 
 
     }
@@ -53,7 +58,7 @@ public class LootManager : MonoBehaviour
     private void OnDestroy()
     {
         if (Event_System.instance != null)
-            Event_System.instance.OnEnemyKilled -= RollMultipuleLoot;
+            Event_System.instance.OnEnemyKilled -= GetOneRandomItemLoot;
     }
 
     public void RegisterLoot(Loot loot)
@@ -118,41 +123,6 @@ public class LootManager : MonoBehaviour
 
 
     }
-
-    public void RollMultipuleLoot(EnemyDamage enemy)
-    {
-        for (int i = 0; i < CalculateLootAmount(); i++)
-        {
-            GetOneRandomItemLoot(enemy);
-        }
-    }
-
-    private int CalculateLootAmount()
-    {
-        float totalWeight = 0;
-        foreach (var weight in amountChanceTable)
-        {
-            totalWeight += weight;
-        }
-
-        float roll = Random.Range(0, totalWeight);
-        float current = 0;
-
-        for (int i = 0; i < amountChanceTable.Count; i++)
-        {
-            current += amountChanceTable[i];
-
-            if (roll < current)
-            {
-                return i + 1;
-            }
-        }
-
-        return 1;
-    }
-
-
-
 
 
 
