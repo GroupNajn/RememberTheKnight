f156d2b1506adfd391941d8c371cd68b50905977
Henric Nilsson <henric.nilsson01@gmail.com>
2026-04-04 00:11:03 +0200

    Revert "LootManager scripted updated"
    This reverts commit 1dd9dbe3b38c07674113eda3519c4d4371083bff.

--- Assets/Scripts/Manager/LootManager.cs
+++ Assets/Scripts/Manager/LootManager.cs
@@ -2,7 +2,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
-using UnityEngine.Rendering;
 
 
 // Script made by Henric some random date
@@ -21,7 +20,10 @@ public class LootManager : MonoBehaviour
     HashSet<Loot> droppedLoot;
     [SerializeField] Loot soulPrefab;
 
-    [SerializeField] List<float> amountChanceTable;
+    //[SerializeField] float oneItemDropChance;
+    //[SerializeField] float twoItemDropChance;
+    //[SerializeField] float threeItemDropChance;
+
 
 
     /*
@@ -37,7 +39,10 @@ public class LootManager : MonoBehaviour
 
     private void Start()
     {
-    
+        //oneItemDropChance = 80.0f;
+        //twoItemDropChance = 15.0f;
+        //threeItemDropChance = 5.0f;
+
         if (Event_System.instance == null)
         {
             Debug.LogError("Event_System.instance is null in LootManager.Start()");
@@ -45,7 +50,7 @@ public class LootManager : MonoBehaviour
         }
 
         //Debug.Log("LootManager subscribed");
-        Event_System.instance.OnEnemyKilled += RollMultipuleLoot;
+        Event_System.instance.OnEnemyKilled += GetOneRandomItemLoot;
 
 
     }
@@ -53,7 +58,7 @@ public class LootManager : MonoBehaviour
     private void OnDestroy()
     {
         if (Event_System.instance != null)
-            Event_System.instance.OnEnemyKilled -= RollMultipuleLoot;
+            Event_System.instance.OnEnemyKilled -= GetOneRandomItemLoot;
     }
 
     public void RegisterLoot(Loot loot)
@@ -67,16 +72,12 @@ public class LootManager : MonoBehaviour
         if (loot != null)
             droppedLoot.Remove(loot);
     }
-    public void RollMultipuleLoot(EnemyDamage enemy)
-    {
-        for (int i = 0; i < CalculateLootAmount(); i++)
-        {
-            GetOneRandomItemLoot(enemy);
-        }    
-    }
 
+    //public void CalculateLootTier(EnemyDamage enemy)
+    //{
+    //    float totalTierWight = 0;
 
-
+    //}
 
     public void GetOneRandomItemLoot(EnemyDamage enemy)
     {
@@ -122,32 +123,6 @@ public class LootManager : MonoBehaviour
 
 
     }
-
-    private int CalculateLootAmount()
-    {
-        float totalWeight = 0;
-        foreach (var weight in amountChanceTable)
-        {
-            totalWeight += weight;
-        }
-
-        float roll = Random.Range(0, totalWeight);
-        float current = 0;
-
-        for (int i = 0; i < amountChanceTable.Count; i++)
-        {
-            current += amountChanceTable[i];
-
-            if (roll < current)
-            {
-                return i + 1; 
-            }
-        }
-
-        return 1; 
-    }
-
-
 
 
 
