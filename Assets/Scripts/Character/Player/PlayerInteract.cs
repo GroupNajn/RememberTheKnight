e000604907a8b60c8a34e86ca87f375ac4e17e6a
Anton Bockman <anton.bockman@gmail.com>
2026-04-08 14:36:11 +0200

    Added interact ray and deathzone damage

--- Assets/Scripts/Character/Player/PlayerInteract.cs
+++ Assets/Scripts/Character/Player/PlayerInteract.cs
@@ -5,12 +5,18 @@ public class PlayerInteract : MonoBehaviour
 {
     private Camera camera;
     public float InteractDistance = 8f;
+    [SerializeField] GameObject interactUI;
     PlayerController playerController;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         playerController = GetComponent<PlayerController>();
         //camera = playerController._playerCamera.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        CheckInteractable();
     }
 
     public void OnInteract()
@@ -29,5 +35,26 @@ public class PlayerInteract : MonoBehaviour
                 interactable.Interact();
             }
         }
+    }
+
+    public void CheckInteractable()
+    {
+        camera = playerController._playerCamera.GetComponent<Camera>();
+
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, InteractDistance, 3))
+        {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                interactUI.SetActive(true);
+                return;
+            }
+        }
+
+        interactUI.SetActive(false);
     }
 }
