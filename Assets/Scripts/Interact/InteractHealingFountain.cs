73cda3dc0d2be2b185e688141bc862fb2556849e
Anton Bockman <anton.bockman@gmail.com>
2026-04-13 18:07:37 +0200

    Improved healing fountain
    Theo added coroutines for light to fade out
    
    Also fixed the healing from fountain properly!

--- Assets/Scripts/Interact/InteractHealingFountain.cs
+++ Assets/Scripts/Interact/InteractHealingFountain.cs
@@ -1,16 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class InteractHealingFountain : MonoBehaviour, IInteractable
 {
-    PlayerStats playerStats;
+    private PlayerStats playerStats;
+
+    [SerializeField] GameObject lightObject;
+    private Light lightSource;
+
+    private Collider interactCollider;
+    [SerializeField] bool isExpended;
     void Start()
     {
         playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        interactCollider = GetComponent<CapsuleCollider>();
+        lightSource = lightObject.GetComponent<Light>();
     }
 
     public void Interact()
     {
-        playerStats.Heal(25f);
+        if (!isExpended)
+        { 
+            playerStats.Heal(25f);
+            interactCollider.enabled = false;
+            isExpended = true;
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        var fog = lightObject.GetComponentInChildren<ParticleSystem>();
+        fog.Stop();
+        while (lightSource.intensity > 0.01f)
+        {
+            lightSource.intensity *= 0.9f;
+            yield return new WaitForSeconds(0.05f);
+        }
+        lightObject.SetActive(false);
+        yield return null;
     }
 
 }
