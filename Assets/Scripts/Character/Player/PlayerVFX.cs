5fe91069230eae35ab608ecbb9ba6526d0875f7b
Jonablixt <jonablixt@gmail.com>
2026-04-22 13:44:00 +0200

    added blood fx, slam fx and changed explotion fx

--- Assets/Scripts/Character/Player/PlayerVFX.cs
+++ Assets/Scripts/Character/Player/PlayerVFX.cs
@@ -4,4 +4,14 @@ public class PlayerVFX : CharacterVFX
 {
     // ONLY NEEDS TO BE EMPTY FOR NOW
     // CAN BE USED FOR PLAYER SPECIFIK VFX
+    [Header("Slam VFX")]
+    public GameObject SlamVFX;
+    public float ForwardOffset = 1f;
+    // public float ForwardOffset;
+
+    public void PlaySlamVFX()
+    {
+        GameObject bloodSplatter = Instantiate(SlamVFX, this.gameObject.transform.position + this.gameObject.transform.forward * ForwardOffset, Quaternion.identity);
+        Destroy(bloodSplatter, 2f);
+    }
 }
