11593431efb2084c0ef0219491aa524982ec5592
Jonablixt <jonablixt@gmail.com>
2026-04-22 17:09:21 +0200

    Added heling vfx, not sure if we will keep it

--- Assets/Scripts/Character/Player/PlayerVFX.cs
+++ Assets/Scripts/Character/Player/PlayerVFX.cs
@@ -7,11 +7,19 @@ public class PlayerVFX : CharacterVFX
     [Header("Slam VFX")]
     public GameObject SlamVFX;
     public float ForwardOffset = 1f;
+
+    [Header("Heal VFX")]
+    public GameObject HealVFX;
     // public float ForwardOffset;
 
     public void PlaySlamVFX()
     {
-        GameObject bloodSplatter = Instantiate(SlamVFX, this.gameObject.transform.position + this.gameObject.transform.forward * ForwardOffset, Quaternion.identity);
-        Destroy(bloodSplatter, 2f);
+        GameObject VFX = Instantiate(SlamVFX, this.gameObject.transform.position + this.gameObject.transform.forward * ForwardOffset, Quaternion.identity);
+        Destroy(VFX, 2f);
+    }
+    public void PlayHealVFX()
+    {
+        GameObject VFX = Instantiate(HealVFX, this.gameObject.transform.position + this.gameObject.transform.forward * ForwardOffset, Quaternion.identity);
+        Destroy(VFX, 3f);
     }
 }
