481f5b9fe773307cde6dbb51977bdfa7a56f033e
Lukas <lukas.karlsson2006@gmail.com>
2026-04-24 19:08:41 +0200

    Second level, damageble wall clean up,
    Second level needs work on secret area

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     // Made by Lukas 2026-03-24
+    // Updated by Lukas 2026-04-24
     [SerializeField] List<GameObject> spawnObjects = new List<GameObject>();
     [SerializeField] float spawnRadius = 10f; // Used if spawnPoints is empty
     [SerializeField] List<Transform> spawnPoints = new List<Transform>();
+    [SerializeField] bool randomObjectSpawns = false;
 
     bool randomSpawnPoints;
     int currentSpawnIndex = 0;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         randomSpawnPoints = spawnPoints.Count == 0;
 
-        Spawn();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        if (randomObjectSpawns)
+        {
+            RandomSpawn();
+        }
+        else
+        {
+            Spawn();
+        }
     }
 
     public void Spawn()
@@ -44,9 +46,31 @@ public class Spawner : MonoBehaviour
                 currentSpawnIndex = (currentSpawnIndex + 1) % spawnPoints.Count;
             }
 
-            Debug.Log("Spawn position = " + spawnPosition);
-
             Instantiate(spawnObjects[i], spawnPosition, Quaternion.identity);
+        }
+    }
+
+    public void RandomSpawn()
+    {
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
+            Vector3 spawnPosition;
+
+            GameObject randomObject = spawnObjects[Random.Range(0, spawnObjects.Count)];
+
+            if (randomSpawnPoints)
+            {
+                float randomX = Random.Range(-spawnRadius, spawnRadius);
+                float randomZ = Random.Range(-spawnRadius, spawnRadius);
+
+                spawnPosition = transform.position + new Vector3(randomX, 0, randomZ);
+            }
+            else
+            {
+                spawnPosition = spawnPoints[i].position;
+            }
+
+            Instantiate(randomObject, spawnPosition, Quaternion.identity);
         }
     }
 }
