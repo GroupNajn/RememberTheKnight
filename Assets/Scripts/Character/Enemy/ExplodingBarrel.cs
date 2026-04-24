1b3d0d67f2a59a1493d6b90ba9d9d4f664941a94
Anton Bockman <anton.bockman@gmail.com>
2026-04-24 19:14:21 +0200

    Fixed level and deathzone mainly
    Fixed first level and deathzone
    
    Fixed ballista enemy
    Fixed global volume in first level!

--- Assets/Scripts/Character/Enemy/ExplodingBarrel.cs
+++ Assets/Scripts/Character/Enemy/ExplodingBarrel.cs
@@ -3,22 +3,14 @@ using UnityEngine;
 public class ExplodingBarrel : MonoBehaviour
 {
     // Made by Lukas 2026-03-20
-    ExplotionTest explotionTest;
+    Explosion explosion;
     void Awake()
     {
-        explotionTest = GetComponent<ExplotionTest>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            Explode();
-        }
+        explosion = GetComponent<Explosion>();
     }
 
     public void Explode()
     {
-        explotionTest.Explode();
+        explosion.OnExplode();
     }
 }
