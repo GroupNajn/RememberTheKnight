bd5ef4570c9fe5fefa681c1379f25896a4896866
Jonablixt <jonablixt@gmail.com>
2026-04-26 00:55:30 +0200

    Fireballs no longer spawn arrow vfx
    you now also take damage when rolling while having a arrow in your body

--- Assets/Scripts/Character/Player/PlayerVFX.cs
+++ Assets/Scripts/Character/Player/PlayerVFX.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class PlayerVFX : CharacterVFX
 {
-    // ONLY NEEDS TO BE EMPTY FOR NOW
-    // CAN BE USED FOR PLAYER SPECIFIK VFX
     [Header("Slam VFX")]
     public GameObject SlamVFX;
     public float ForwardOffset = 1f;
@@ -12,7 +10,6 @@ public class PlayerVFX : CharacterVFX
     public GameObject HealVFX;
     [Header("Arrow VFX")]
     public GameObject Arrow_VFX;
-    // public float ForwardOffset;
 
     public void PlaySlamVFX()
     {
@@ -24,10 +21,28 @@ public class PlayerVFX : CharacterVFX
         GameObject VFX = Instantiate(HealVFX, this.gameObject.transform.position + this.gameObject.transform.forward * ForwardOffset, Quaternion.identity);
         Destroy(VFX, 3f);
     }
-
     public void PlayArrowVFX(Vector3 contactPoint)
-    {   GameObject root = GameObject.FindWithTag("Root");
+    {
+        GameObject root = GameObject.FindWithTag("Root");
         GameObject VFX = Instantiate(Arrow_VFX, contactPoint, Quaternion.LookRotation(contactPoint - root.transform.position) * Quaternion.Euler(0, 180, 0), root.transform);
         Destroy(VFX, 10f);
+    }
+
+    public void Update()
+    {
+        //if arrows in player body, take damage when rolling
+
+        if (this.gameObject.GetComponent<PlayerStates>().CurrentMoveState == MoveState.Dodging)
+        {
+            foreach (Transform child in GameObject.FindWithTag("Root").GetComponentsInChildren<Transform>())
+            {
+                if (child.CompareTag("Arrow_VFX"))
+                {
+                    PlayerManager playerManager = this.gameObject.GetComponent<PlayerManager>();
+                    playerManager.TakeDamage(5, child.position);
+                    Destroy(child.gameObject);
+                }
+            }
+        }
     }
 }
