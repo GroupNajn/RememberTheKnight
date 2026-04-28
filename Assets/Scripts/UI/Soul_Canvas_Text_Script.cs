cebe4a8694c3ac2a15f1996fc376a3daff719549
Lukas <lukas.karlsson2006@gmail.com>
2026-04-28 11:16:11 +0200

    Canvas camera

--- Assets/Scripts/UI/Soul_Canvas_Text_Script.cs
+++ Assets/Scripts/UI/Soul_Canvas_Text_Script.cs
@@ -1,32 +1,20 @@
 using UnityEngine;
 using TMPro;
+
 public class Soul_Canvas_Text_Script : MonoBehaviour
 {
     int soulAmount = 0;
     TextMeshProUGUI tmp;
-    private bool canIncreaseSouls = true;
 
     void Start()
     {
         tmp = GetComponentInChildren<TextMeshProUGUI>();
         tmp.text = soulAmount.ToString();
-        GetComponent<Canvas>().worldCamera = Camera.main;
     }
-
-    private void OnDisable()
-    {
-       
-    }
-
     public void SetSoulsAmount(int souls)
     {
        
         tmp.text = souls.ToString();
       
     }
-
-
-
-    
-
 }
