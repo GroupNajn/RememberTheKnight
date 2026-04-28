d3235d16a748900dbfcfc0b2abb1101e3cf1420a
Anton Bockman <anton.bockman@gmail.com>
2026-04-28 13:06:18 +0200

    Fixed random poster

--- Assets/Scripts/ShopSlotCard.cs
+++ Assets/Scripts/ShopSlotCard.cs
@@ -5,18 +5,22 @@ public class ShopSlotCard : MonoBehaviour
 {
     [SerializeField] private bool unlocked;
 
-    private List<GameObject> posters = new ();
+    private ShopBoard board;
 
     void Start()
     {
-        posters = GetComponentInParent<List<GameObject>>();
+        board = GetComponentInParent<ShopBoard>();
     }
     public void SetCard(CardData card)
     {
         if (!unlocked)
         {
+            int randomIndex = Random.Range(0, board.randomPosters.Count);
+            GameObject randomPoster = board.randomPosters[randomIndex];
 
+            Instantiate(randomPoster, this.transform);
         }
+
         Debug.Log($"Slot {name} card {card.name}");
 
         Instantiate(card, this.transform);
