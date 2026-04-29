908fbd28ef7b83d729629cc0be522e20bbb594c6
Kodekingen <lukas.karlsson2006@gmail.com>
2026-04-29 17:02:11 +0200

    Second level Mexico, better lobby transition
    Second level main island has been rotated 180 deg, with a an orange tint and a lack of foilage.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance == null)
+        if (Instance == null || Instance != this)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
