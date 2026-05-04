4108409f9ab95225eef3fa32db79e004a72eb531
Theo Johansson <git@theojohansson.dev>
2026-05-04 20:58:20 +0200

    Created 2 actions that rotates a NavmeshAgent according to its angular speed
    Can either rotate by a fixed angle or rotate towards a transform

---
+++ Assets/Scripts/Behaviour/NavAgentRotateAngleAction.cs
@@ -0,0 +1,34 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using UnityEngine.AI;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "NavAgent Rotate Angle", story: "Rotates [Self] by [Angle] ° using angular speed", category: "Action", id: "86ea72e17ccda1be73ea51613314dc6a")]
+public partial class NavAgentRotateAngleAction : Action
+{
+    [SerializeReference] public BlackboardVariable<NavMeshAgent> Self;
+    [SerializeReference] public BlackboardVariable<float> Angle;
+    private float remainingDegrees;
+    protected override Status OnStart()
+    {
+        remainingDegrees = Mathf.Abs(Angle.Value);
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        if (remainingDegrees <= 0) return Status.Success;
+        Self.Value.transform.Rotate(Self.Value.transform.up, Angle.Value * Time.deltaTime);
+        remainingDegrees -= Mathf.Abs(Angle.Value * Time.deltaTime);
+        return Status.Running;
+    }
+
+    protected override void OnEnd()
+    {
+
+    }
+}
+
