6dd90f67a70b7399dc0da19d1be79b913daf6608
Lukas <lukas.karlsson2006@gmail.com>
2026-05-04 21:22:04 +0200

    Cup material reset, crystalball text

--- Assets/Scripts/UI/CardShopUI.cs
+++ Assets/Scripts/UI/CardShopUI.cs
@@ -1,9 +1,7 @@
-using Mono.Cecil.Cil;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class CardShopUI : MonoBehaviour
 {
