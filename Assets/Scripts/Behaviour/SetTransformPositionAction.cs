2ed00f5ae0446c9ec81e16960cb349b41ad7bd6d
Theo Johansson <git@theojohansson.dev>
2026-05-04 21:02:02 +0200

    Created an action that can set the position of a transform
    If no transform is provided, creates one dynamically

---
+++ Assets/Scripts/Behaviour/SetTransformPositionAction.cs
@@ -0,0 +1,39 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "SetTransformPosition", story: "Set [Postion] in [Transform]", category: "Action", id: "fd6d9cd035fef401c5b9b030491ba22c")]
+public partial class SetTransformPositionAction : Action
+{
+    [SerializeReference] public BlackboardVariable<Vector3> Postion;
+    [SerializeReference] public BlackboardVariable<Transform> Transform;
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    GameObject transform;
+    protected override Status OnStart()
+    {
+
+        if (Transform.Value == null)
+        {
+            transform = new($"{Self.Value.name} Target");
+            transform.transform.position = Self.Value.transform.position;
+            Transform.Value = transform.transform;
+
+        }
+        if (Postion.Value == null) return Status.Failure;
+        Transform.Value.position = Postion.Value;
+        return Status.Success;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
