5025f6c951ad4f859292b2f47bcf26dc0f13d3cd
Theo Johansson <git@theojohansson.dev>
2026-05-04 21:00:48 +0200

    Improvments to the line of sight action

--- Assets/Scripts/Behaviour/LineOfSightAction.cs
+++ Assets/Scripts/Behaviour/LineOfSightAction.cs
@@ -3,43 +3,52 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using System.Linq;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "LineOfSight", story: "Sets [NavigationTarget] when [Target] is in sight", category: "Action", id: "462f441212c9758ad05609aae132293a")]
+[NodeDescription(name: "Line of Sight", story: "Sets [HasSight] and [TargetPosition] when [Target] is in sight", category: "Action", id: "462f441212c9758ad05609aae132293a")]
 public partial class LineOfSightAction : Action
 {
-    [SerializeReference] public BlackboardVariable<Transform> NavigationTarget;
-    [SerializeReference] public BlackboardVariable<GameObject> Target;
-    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<bool> HasSight;
+    [SerializeReference] public BlackboardVariable<Vector3> TargetPosition;
+    [SerializeReference] public BlackboardVariable<Transform> Target;
+    [SerializeReference] public BlackboardVariable<Transform> Self;
     [SerializeReference] public BlackboardVariable<float> ViewAngle = new(45);
-    GameObject navigationTarget;
+    [SerializeReference] public BlackboardVariable<float> MaxDistance = new(100f);
+    [SerializeReference] public BlackboardVariable<string> TargetTag = new("Player");
+    [SerializeReference] public BlackboardVariable<int> ExcludeLayer;
+
     protected override Status OnStart()
     {
-        if (Self.Value == null) return Status.Failure;
-        if (NavigationTarget.Value == null)
+        if (Self.Value == null || Target.Value == null) return Status.Failure;
+
+
+        var forward = Self.Value.forward;
+        var dir = (Target.Value.position - Self.Value.position).normalized;
+
+        float angle = Vector3.Dot(forward, dir);
+
+        if (angle < Mathf.Cos(Mathf.Deg2Rad * ViewAngle.Value / 2))
         {
-            navigationTarget = new("Navigation Target");
-            navigationTarget.transform.parent = Self.Value.transform;
-            navigationTarget.transform.localPosition = Vector3.zero;
-            NavigationTarget.Value = navigationTarget.transform;
+            HasSight.Value = false;
+            return Status.Success;
         }
-        return Status.Running;
+
+        LayerMask excludeEnemies = ~(1 << ExcludeLayer.Value);
+        if (Physics.Raycast(Self.Value.position, dir, out RaycastHit hit, MaxDistance.Value, excludeEnemies))
+        {
+            if (hit.collider.CompareTag(TargetTag.Value))
+            {
+                HasSight.Value = true;
+                TargetPosition.Value = Target.Value.position;
+            }
+        }
+        return Status.Success;
     }
 
     protected override Status OnUpdate()
     {
 
-
-        var forward = Self.Value.transform.forward;
-        var dir = (Target.Value.transform.position - Self.Value.transform.position).normalized;
-        float angle = Mathf.Rad2Deg * Mathf.Acos(
-            Vector2.Dot(
-                new Vector2(forward.x, forward.z),
-                new Vector2(dir.x, dir.z)
-            )
-        );
-        if (Mathf.Abs(angle) <= ViewAngle.Value)
-            NavigationTarget.Value.position = Target.Value.transform.position;
         return Status.Success;
     }
 
