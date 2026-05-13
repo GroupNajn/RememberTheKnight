724b5e0e3f5750c12d1f06fe62be808d9d0a2232
Henric Nilsson <henric.nilsson01@gmail.com>
2026-05-13 23:02:42 +0200

    Adding a new UI Prefab
    Adding a new UI Prefab, also changing scripts and adding new ones to accomedate the new UI.

---
+++ Assets/CardPickupUI.cs
@@ -0,0 +1,161 @@
+
+using System.Collections;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardPickupUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI tierTMP;
+    [SerializeField] TextMeshProUGUI familyTMP;
+    [SerializeField] TextMeshProUGUI infoBoxTMP;
+    [SerializeField] CardData cardData;
+    [SerializeField] AnimationCurve bounceCurve;
+    [SerializeField] Transform pickupWindowTransform;
+    [SerializeField] GameObject pickupPrefab;
+
+    private Vector3 targetScale;
+   [SerializeField] private float duration = 1f;
+    void Start()
+    {
+        this.transform.localScale = new Vector3(0, 0, 0);
+        targetScale = new Vector3(1, 1, 1);
+    }
+
+    private void OnEnable()
+    {
+        SetFamilyText();
+        SetTierText();
+        SetInfoBoxText();
+        DisableButtons();
+        StartCoroutine(ScaleBouncePickUpWindow());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnConfirmPickup()
+    {
+
+    }
+
+    public void OnSacrificeCard()
+    {
+        Event_System.instance.OnDroopMultipleSouls.Invoke(cardData);
+    }
+
+    /// <summary>
+    /// Animates the pick-up window's scale using a bounce effect over a set duration.
+    /// </summary>
+    /// <remarks>This coroutine should be started to visually animate the pick-up window with a bounce scaling
+    /// effect. The window's scale is updated each frame according to the specified animation curve. At the end of the
+    /// animation, the window's scale is reset and related buttons are enabled.</remarks>
+    /// <returns>An enumerator that performs the scale animation when iterated.</returns>
+    private IEnumerator ScaleBouncePickUpWindow()
+    {
+        float timer = 0f;
+
+        while (timer< duration)
+        {
+            timer += Time.deltaTime;
+
+            float t = timer / duration;
+            float curvevalue = bounceCurve.Evaluate(t);
+
+            pickupWindowTransform.localScale = Vector3.one * curvevalue;
+
+            yield return null;
+        }
+        pickupWindowTransform.localScale = Vector3.one;
+        EnableButtons();
+    }
+
+    private void DisableButtons()
+    {
+        foreach (Button button in GetComponentsInChildren<Button>())
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+
+    private void EnableButtons()
+    {
+        foreach(Button button in GetComponentsInChildren<Button>())
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
+    /// <summary>
+    /// Sets the current card data for the instance. 
+    /// Use this inside of UIManager when deciding if CardPickupUI should be enabled.
+    /// Failsafe to set the card and if it is set, enable CardPickupUI
+    /// </summary>
+    /// <param name="card">The card data to assign. Cannot be null.</param>
+    /// <returns>true if the card data was set successfully; otherwise, false.</returns>
+    public bool SetCardData(CardData card)
+    {
+        return (cardData = card) != null;
+    }
+    private void SetFamilyText()
+    {
+        StringBuilder familyText = new StringBuilder();
+        familyText.AppendLine("Family");
+
+        familyText.Append($"{cardData.cardFamily}");
+        familyTMP.text = familyText.ToString();
+
+    }
+
+    private void SetTierText()
+    {
+
+        StringBuilder tierText = new StringBuilder();
+        tierText.AppendLine("Tier");
+
+        tierText.Append($"{cardData.cardTier}");
+        tierTMP.text = tierText.ToString();
+    }
+    private void SetInfoBoxText()
+    {
+        StringBuilder stats = new StringBuilder();
+
+        if (cardData.healthModifier > 0)
+            stats.AppendLine($"Health + {cardData.healthModifier}");
+
+        if (cardData.staminaModifier > 0)
+            stats.AppendLine($"Stamina + {cardData.staminaModifier}");
+
+        if (cardData.luckModifier > 0)
+            stats.AppendLine($"Luck + {cardData.luckModifier}%");
+
+        if (cardData.damageModifier > 0)
+            stats.AppendLine($"Damage + {cardData.damageModifier * 100}%");
+
+        if (cardData.critChance > 0)
+            stats.AppendLine($"critical chance + {cardData.critChance}%");
+
+        if (cardData.walkSpeedModifier > 0)
+            stats.AppendLine($"walk speed + {cardData.walkSpeedModifier}");
+
+        if (cardData.sprintSpeedModifier > 0)
+            stats.AppendLine($"sprint speed + {cardData.sprintSpeedModifier}%");
+
+        if (cardData.dodgeSpeedModifier > 0)
+            stats.AppendLine($"dodge speed + {cardData.dodgeSpeedModifier}%");
+
+        if (cardData.healModifier > 0)
+            stats.AppendLine($" heal multiplier + {cardData.healModifier}%");
+
+        if (cardData.knockbackModifier > 0)
+            stats.AppendLine($"resistance + {cardData.knockbackModifier}%");
+
+        if (cardData.weaponSize != Vector3.zero)
+            stats.AppendLine($"weapon size + {cardData.weaponSize.y * 10}");
+
+        infoBoxTMP.text = stats.ToString();
+    }
+
+}
--- Assets/Scripts/Manager/Event_System.cs
+++ Assets/Scripts/Manager/Event_System.cs
@@ -35,6 +35,7 @@ public class Event_System : MonoBehaviour
     public Action<Loot> OnLootPickedUp;
     public Action<Loot> OnGetLoot;
     public Action<IDamageable> OnSpawnChargedSoul;
+    public Action<CardData> OnCardPickUp;
 
     //Souls Related
     public Action<int> OnSoulsSpent;
