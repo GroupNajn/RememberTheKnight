8b86cda2fc91162c6eabfe8edf38722d0c29d26e
Theo Johansson <git@theojohansson.dev>
2026-05-13 04:33:25 +0200

    Removed using directives that prevented build

--- Assets/Scripts/UI/CardUI.cs
+++ Assets/Scripts/UI/CardUI.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Text;
 using TMPro;
 using Unity.Cinemachine;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -255,7 +254,7 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         cardImage.rectTransform.localScale = new Vector3(1, 1, 1);
     }
 
-  
+
     IEnumerator UnFlipCard()
     {
         //yield return StartCoroutine(FlipCard());
