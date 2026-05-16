dac0ef1b73ba5efafc4d9a3dd292330f6e2566e6
Henric Nilsson <henric.nilsson01@gmail.com>
2026-05-16 20:11:21 +0200

    Fixed Ui Prefabs

--- Assets/ButtonHover.cs
+++ Assets/ButtonHover.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ButtonHover : MonoBehaviour
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private RectTransform rect;
+    LTDescr currentTween;
+    private int tweenID;
+
     void Start()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+   
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        LeanTween.cancel(tweenID);
+
+        tweenID = LeanTween.scale(rect, Vector3.one * 1.05f, 0.2f).setEaseOutQuad().setIgnoreTimeScale(true).id;
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.cancel(tweenID);
+
+        tweenID = LeanTween.scale(rect, Vector3.one, 0.2f).setEaseOutQuad().setIgnoreTimeScale(true).id;
     }
 }
