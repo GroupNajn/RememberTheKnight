9861ee4e6888efb280e6e3f0e650eeea683fcac7
Henric Nilsson <henric.nilsson01@gmail.com>
2026-05-17 23:31:36 +0200

    Adding spline to donation well
    Added souls being instantiated from the player when interacting with the donation well. The souls are being moved with a spline.

--- Assets/DonationMoveSoul.cs
+++ Assets/DonationMoveSoul.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -8,26 +9,39 @@ public class DonationMoveSoul : MonoBehaviour
     [SerializeField] private GameObject soulPrefab;
     [SerializeField] private SplineContainer spline;
     [SerializeField] private float moveDuration = 3f;
+    [SerializeField] private Transform playerTransform;
+    private bool hasInserted = false;
     void Start()
     {
-        
+        playerTransform = GameObject.Find("PlayerLookAt").transform;
     }
 
     void Update()
     {
-        
+
     }
 
 
     public void InstantiateSoul()
     {
-        GameObject soul = Instantiate(soulPrefab);
+        Vector3 localPosition = spline.transform.InverseTransformPoint(playerTransform.position);
 
+        BezierKnot playerKnot = new BezierKnot((float3)localPosition);
+        if (!hasInserted)
+        {
+            spline.Spline.Insert(0, playerKnot);
+            hasInserted = true;
+        }
+        else spline.Spline[0] = playerKnot;
+
+            GameObject soul = Instantiate(soulPrefab);
         DonationSoulMover mover = soul.GetComponent<DonationSoulMover>();
 
         if (mover == null)
             mover = soul.AddComponent<DonationSoulMover>();
 
+
         mover.Initialize(spline, moveDuration);
+        
     }
 }
