c2218a2f8104579b582fa32670e3a3bd0eaef24b
WilmerJonsson <wimse03@gmail.com>
2026-05-19 11:37:56 +0200

    bossbar lerp

--- Assets/BossBar.cs
+++ Assets/BossBar.cs
@@ -8,6 +8,10 @@ public class BossBar : MonoBehaviour
 
     [SerializeField] Slider bossBar;
 
+    [SerializeField] float lerpSpeed = 2f;
+    public RectTransform lerpingRectTransform;
+    bool barSetupComplete = false;
+
     private void Start()
     {
         Debug.Log("Start called in BossBar");
@@ -22,8 +26,12 @@ public class BossBar : MonoBehaviour
         Boss = GameObject.FindGameObjectWithTag("Boss");
         enemyDamage = Boss.GetComponent<EnemyDamage>();
         Debug.Log("Stuff found");
+
         bossBar.maxValue = enemyDamage.MaxHealth;
         bossBar.value = enemyDamage.Health;
+
+        enemyDamage.OnHealthChanged += OnHealthChanged;
+        barSetupComplete = true;
     }
 
     public void ShowBossBar()
@@ -38,5 +46,28 @@ public class BossBar : MonoBehaviour
         bossBar.gameObject.SetActive(false);
     }
 
+    public void OnHealthChanged(float current, float max)
+    {
+        bossBar.value = current;
+    }
 
+    private void Update() // Lerp the healthbar fill to the target position
+    {
+        if (barSetupComplete)
+        {
+            bool lerpCondition = lerpingRectTransform.anchorMax.x > bossBar.fillRect.anchorMax.x || lerpingRectTransform.anchorMin.x < bossBar.fillRect.anchorMin.x;
+
+            if (lerpCondition)
+            {
+                lerpingRectTransform.anchorMax = Vector2.Lerp(lerpingRectTransform.anchorMax, bossBar.fillRect.anchorMax, Time.deltaTime * lerpSpeed);
+                lerpingRectTransform.anchorMin = Vector2.Lerp(lerpingRectTransform.anchorMin, bossBar.fillRect.anchorMin, Time.deltaTime * lerpSpeed);
+            }
+            else
+            {
+                lerpingRectTransform.anchorMax = bossBar.fillRect.anchorMax;
+                lerpingRectTransform.anchorMin = bossBar.fillRect.anchorMin;
+            }
+        }
+
+    }
 }
