e902494ea567f5d9f48bdacae51819869f4c11d3
Lukas <lukas.karlsson2006@gmail.com>
2026-05-20 16:29:54 +0200

    Revert "Random crystal ball spawns" spawner folder rename

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -114,6 +114,7 @@ public class Spawner : MonoBehaviour
 
             if (Random.Range(0f, 1f) > spawnChance)
             {
+                Debug.Log("Doesn't spawn object");
                 continue;
             }
 
