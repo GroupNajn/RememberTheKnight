0a1390cfa4a77da46e4771d00a302b74d8ab47ab
Henric Nilsson <henric.nilsson01@gmail.com>
2026-05-25 19:50:00 +0200

    Adding boss loot
    Adding loot to be spawned when the boss is killed.

--- Assets/Scripts/Loot/Souls/ChargedSoul.cs
+++ Assets/Scripts/Loot/Souls/ChargedSoul.cs
@@ -1,5 +1,13 @@
+using FMODUnity;
 using UnityEngine;
 
 public class ChargedSoul : Soul, IPickupable
 {
+
+    public override void Pickup()
+    {
+        RuntimeManager.PlayOneShotAttached(soulPickupEvent, playerTransform.gameObject);
+
+        base.Pickup();
+    }
 }
