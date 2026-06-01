2906f902f7cf599c64183c578e3403d46c051b7e
Henric Nilsson <henric.nilsson01@gmail.com>
2026-06-01 16:57:22 +0200

    Fixed SecretCard instantiate
    Fixed the secretCard instantiate and the save feature when the card is picked up. So that the does not spawn anymore if it got picked up during any of the players serssions.
    
    The playerprefs need to be reset for it to respawn.

--- Assets/Scripts/Data/PlayerPrefsSaveSystem.cs
+++ Assets/Scripts/Data/PlayerPrefsSaveSystem.cs
@@ -70,6 +70,11 @@ public static class PlayerPrefsSaveSystem
         PlayerPrefs.SetInt(soulValueText, amount);
     }
 
+    public static void SaveSecretCardPickup(string secretCardString)
+    {
+        PlayerPrefs.SetInt(secretCardString, 1);
+    }
+
     public static void SaveSoulsDonatedToFamily(int amount)
     {
         //PlayerPrefs.
