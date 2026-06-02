4c8c5b50a68da422542691ce1eafe58a77011740
Anton Bockman <anton.bockman@gmail.com>
2026-06-02 22:58:43 +0200

    Fixed more lore lines
    Propperly fixed secret

--- Assets/Scripts/UI/Books/InteractableLore.cs
+++ Assets/Scripts/UI/Books/InteractableLore.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class InteractableLore : MonoBehaviour, IInteractable, IInteractableUIText
 {
+<<<<<<< Updated upstream
     [Header("Saved Data")]
     [SerializeField] private string interactableID;
     [SerializeField] private GameObject firstTimeEffect;
+=======
+    // created by Anton
+>>>>>>> Stashed changes
 
     [Header("Lore")]
     [SerializeField] private LoreEntry loreEntry;
@@ -22,6 +26,7 @@ public class InteractableLore : MonoBehaviour, IInteractable, IInteractableUITex
         loreManager = FindFirstObjectByType<LoreManager>();
         lorePageUI = FindFirstObjectByType<LorePageUI>(FindObjectsInactive.Include);
 
+<<<<<<< Updated upstream
         PlayerPrefs.DeleteAll(); // Remove this line after testing to keep player progress
 
         if (InteractableSaveSystem.HasInteracted(interactableID))
@@ -29,16 +34,25 @@ public class InteractableLore : MonoBehaviour, IInteractable, IInteractableUITex
             if (firstTimeEffect != null)
                 firstTimeEffect.SetActive(false);
         }
+=======
+        //PlayerPrefs.DeleteAll(); // Remove this line after testing to keep player progress
+>>>>>>> Stashed changes
     }
 
     public void Interact()
     {
+<<<<<<< Updated upstream
         if (!InteractableSaveSystem.HasInteracted(interactableID))
         {
             InteractableSaveSystem.SetInteracted(interactableID);
 
             if (firstTimeEffect != null)
                 firstTimeEffect.SetActive(false);
+=======
+        if (!PlayerPrefsSaveSystem.HasInteracted(loreEntry.id))
+        {
+            PlayerPrefsSaveSystem.SetSaveState(loreEntry.id);
+>>>>>>> Stashed changes
         }
 
         loreManager.UnlockLore(loreEntry.id);
